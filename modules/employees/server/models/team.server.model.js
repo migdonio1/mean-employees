5ebe4910cd34a32abecef73becdf1ace8db4ecd7
Raul Migdonio Rodriguez Te <raulmigdonio@gmail.com>
2016-10-04 19:43:51 -0500

    Se agrego el parametro created al modelo de los empleados y se creo el modelo de equipos

--- modules/employees/server/models/team.server.model.js
+++ modules/employees/server/models/team.server.model.js
@@ -1,46 +1,28 @@
 /**
  * Created by migdonio1 on 10/4/16.
  */
-
 'use strict'
 
 var moment = require('moment-timezone'),
     mongoose  = require('mongoose'),
     Schema = mongoose.Schema;
 
-
-
-
-var EmployeeSchema = new Schema({
+var TeamSchema = new Schema({
     name: {
         type: String,
         default: '',
         trim: true,
         required: 'El nombre no puede estar en blanco'
     },
-    lastName: {
+    description: {
         type: String,
         default: '',
-        trim: true,
-        required: 'El apellido no puede estar vacio'
-    },
-    birthday: {
-        type: Date,
-        default: moment().tz("America/Los_Angeles").format()
-    },
-    salary: {
-        type: number,
-        default: 0,
-        min: 0
+        trim: true
     },
     created: {
         type: Date,
         default: moment().tz("America/Los_Angeles").format()
     },
-    team: {
-        type: Scheme.ObjectId,
-        ref: 'Team'
-    }
 });
 
-mongoose.model('Employee', EmployeeSchema);
+mongoose.model('Team', TeamSchema);
