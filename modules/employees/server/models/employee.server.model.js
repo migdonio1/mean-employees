2c3d51b68401f51df8a196b840ac2671a39eb0cc
Raul Migdonio Rodriguez Te <raulmigdonio@gmail.com>
2016-10-04 19:35:42 -0500

    Se agrego el modelo de empleados y la libreria moment

---
+++ modules/employees/server/models/employee.server.model.js
@@ -0,0 +1,42 @@
+/**
+ * Created by migdonio1 on 10/4/16.
+ */
+
+'use strict'
+
+var moment = require('moment-timezone'),
+    mongoose  = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+
+
+var EmployeeSchema = new Schema({
+    name: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'El nombre no puede estar en blanco'
+    },
+    lastName: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'El apellido no puede estar vacio'
+    },
+    birthday: {
+        type: Date,
+        default: moment().tz("America/Los_Angeles").format()
+    },
+    salary: {
+        type: number,
+        default: 0,
+        min: 0
+    },
+    team: {
+        type: Scheme.ObjectId,
+        ref: 'Team'
+    }
+});
+
+mongoose.model('Employee', EmployeeSchema);
